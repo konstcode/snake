bcfabe68f61d9810efc95fc23b2fc40a07657495
Kostinatyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2024-05-20 14:28:18 +0300

    check and test rodio library

---
+++ src/audio.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::fs;
+use std::fs::File;
+use std::io::{BufReader};
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
+use log::{debug, info};
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
+
+#[derive(Debug)]
+pub struct Records {
+    all: HashMap<String, PathBuf>,
+}
+
+impl Records {
+    pub fn new() -> Self {
+       let records = Records {
+           all: HashMap::new(),
+       };
+       records
+    }
+
+    pub fn init(&mut self) {
+        info!("Loading all audio files...");
+        fs::read_dir("audio").unwrap().for_each(|entry| {
+            let entry = entry.unwrap();
+            if let Ok(file_type) = entry.file_type() {
+                if file_type.is_file() {
+                    self.add(&entry.path());
+                }
+            }
+        });
+    }
+
+    fn add(& mut self, path: &PathBuf) {
+        let string = path.file_stem().unwrap().to_str().unwrap();
+        self.all.insert(String::from(string), path.to_owned());
+        println!("{:?}", self.all);
+    }
+
+    pub fn play(&self, name: &str) {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let path = self.all.get(&String::from(name)).unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        let file = BufReader::new(File::open(path).unwrap());
+        let source = Decoder::new(file).unwrap();
+        sink.append(source);
+
+        sink.play();
+        sink.sleep_until_end();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use std::time::Duration;
-use rodio::{OutputStream, Sink, Source};
-use rodio::source::SineWave;
+use snake::audio::Records;
 
 fn main() {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+    let mut audio = Records::new();
+    audio.init();
+    audio.play("win");
 
-    for i in 0..10 {
-        // Add a dummy source of the sake of the example.
-        let source = SineWave::new(100.0*i as f32)
-            .take_duration(Duration::from_secs_f32(1.0)).amplify(0.40);
-        sink.append(source);
-
-        sink.sleep_until_end();
-    }
 }
