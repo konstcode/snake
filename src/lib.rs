01ec2e850f72f957049bba180277b22cbf38fafd
Kostinatyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2024-06-02 17:32:57 +0300

    add timer module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod audio;
 pub mod frame;
 pub mod render;
 pub mod snake;
+pub mod timer;
 
 pub const NUM_ROWS: usize = 30;
 pub const NUM_COLS: usize = 50;
+
---
+++ src/timer.rs
@@ -0,0 +1,53 @@
+use std::time::Duration;
+
+pub struct Timer {
+    total: Duration,
+    max: Duration,
+    finished: bool,
+}
+
+impl Timer {
+    pub fn new(time: Duration) -> Self {
+        Self {
+            total: Duration::new(0, 0),
+            max: time,
+            finished: false,
+        }
+    }
+
+    pub fn tick(&mut self, time: Duration) {
+        self.total += time;
+        match self.total >= self.max {
+            true => self.finished = true,
+            _ => (),
+        };
+    }
+
+    pub fn finished(&self) -> bool {
+        self.finished
+    }
+
+    pub fn reset(&mut self) {
+        self.total = Duration::new(0, 0);
+        self.finished = false;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::Duration;
+
+    use crate::timer::Timer;
+
+    #[test]
+    fn test_timer() {
+        let mut timer = Timer::new(Duration::new(1, 0));
+        timer.tick(Duration::from_millis(500));
+        assert!(!timer.finished());
+        timer.tick(Duration::from_millis(500));
+        assert!(timer.finished());
+        println!("{:?}", timer.total);
+        timer.reset();
+        assert!(!timer.finished());
+    }
+}
