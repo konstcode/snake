3dc9ec25fc29bcd60b596827462341a46e28b2db
Kostinatyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2024-06-11 23:33:07 +0300

    add methods to increase/decrease & switch options

--- src/menu.rs
+++ src/menu.rs
@@ -28,10 +28,50 @@ impl Menu {
         }
     }
 
-    fn insrease_speed(&mut self) {
+    pub fn insrease_current_option(&mut self) {
+        match self.current_option {
+            MenuOption::Speed => self.increase_speed(),
+            MenuOption::Apples => self.increase_apples(),
+        }
+    }
+    pub fn decrease_current_option(&mut self) {
+        match self.current_option {
+            MenuOption::Speed => self.decrease_speed(),
+            MenuOption::Apples => self.decrease_apples(),
+        }
+    }
+    pub fn switch_current_option(&mut self) {
+        self.current_option = match self.current_option {
+            MenuOption::Speed => MenuOption::Apples,
+            MenuOption::Apples => MenuOption::Speed,
+        }
+    }
+
+    fn increase_speed(&mut self) {
         let new_speed = self.speed_millis + 50;
-        if new_speed < SPEED_LIMITS.1 {
+        if new_speed <= SPEED_LIMITS.1 {
             self.speed_millis += new_speed;
+        }
+    }
+
+    fn increase_apples(&mut self) {
+        let new_max_apples = self.max_apples + 1;
+        if new_max_apples <= APPLE_LIMITS.1 {
+            self.max_apples += new_max_apples;
+        }
+    }
+
+    fn decrease_speed(&mut self) {
+        let new_speed = self.speed_millis - 50;
+        if new_speed >= SPEED_LIMITS.0 {
+            self.speed_millis -= new_speed;
+        }
+    }
+
+    fn decrease_apples(&mut self) {
+        let new_max_apples = self.max_apples - 1;
+        if new_max_apples >= APPLE_LIMITS.0 {
+            self.max_apples -= new_max_apples;
         }
     }
 }
@@ -40,6 +80,7 @@ impl Drawable for Menu {
     fn draw(&self, frame: &mut crate::frame::Frame) {
         // Speed option
         draw_text(frame, Point::new(10, 10), "Speed:");
+
         draw_text(frame, Point::new(10, 11), "Speed:");
     }
 }
